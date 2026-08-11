9a8954acfb6cadf62556ce3fe7e542ab6dd682de
Brian Wilhite <brianwilhite@Brians-MacBook-Pro-3.local>
2026-08-11 10:15:53 -0700

    Temporarily hardcode Supabase keys to fix production

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://cvrzieobtvakfiqgouhw.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2cnppZW9idHZha2ZpcXFvdWh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYzNjkyMjcsImV4cCI6MjAyMjIxMjgzMH0.KXZhfVlY68C9Tuxd54ELhvmn_NhQBX25iw8taEflj3A'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
