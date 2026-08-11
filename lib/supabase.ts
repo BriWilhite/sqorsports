b77136d880df0f3b36cac25d53b732be15691f54
Brian Wilhite <brianwilhite@Brians-MacBook-Pro-3.local>
2026-08-11 10:41:45 -0700

    Fix Supabase key to single line

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,5 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://cvrzieobtvakfiqgouhw.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2cnppZW9idHZha2ZpcXFvdWh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYzNjkyMjcsImV4cCI6MjAyMjIxMjgzMH0.KXZhfVlY68C9Tuxd54ELhvmn_NhQBX25iw8taEflj3A'
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
